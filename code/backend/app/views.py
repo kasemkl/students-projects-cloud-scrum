6a15c3e0036fbdb99477be6a8730dceb90a80221
kasemkl <kmak979@gmail.com>
2023-12-01 23:11:56 +0300

    update code files

--- code/backend/app/views.py
+++ code/backend/app/views.py
@@ -21,47 +21,6 @@ from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from .permissions import *
 from .firebase import *
 from .validition import *
-@api_view(['GET','POST'])
-def home(request):
-    Data=getData('data')
-    return Response(Data)
-
-
-
-class UserLogin(APIView):
-	permission_classes = (permissions.AllowAny,)
-	authentication_classes = (SessionAuthentication,)
-	##
-	def post(self, request):
-		data = request.data
-		serializer = UserLoginSerializer(data=data)
-		if serializer.is_valid(raise_exception=True):
-			user = serializer.check_user(data)
-			login(request, user)
-			return Response({"value":user.is_authenticated}, status=status.HTTP_200_OK)
-
-
-
-class UserRegister(APIView):
-	permission_classes = (permissions.AllowAny,)
-	def post(self, request):
-		clean_data = request.data
-		serializer = UserRegisterSerializer(data=clean_data)
-		if serializer.is_valid(raise_exception=True):
-			user = serializer.create(clean_data)
-			if user:
-				return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-
-class UserLogout(APIView):
-	permission_classes = (permissions.AllowAny,)
-	authentication_classes = ()
-	def post(self, request):
-		logout(request)
-		return Response(status=status.HTTP_200_OK)
-
 
 
 @api_view(['GET', 'POST'])
